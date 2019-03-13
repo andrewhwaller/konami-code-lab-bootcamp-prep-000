20670dde4a1c1cad7bad85e1629797ec6706ac86
Andrew Waller <andrewhwaller@gmail.com>
2019-03-13 00:00:51 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,24 @@ const codes = [
   "a"
 ];
 
+let index = 0
+
 function init() {
-  // your code here
+  
+}
+
+function onKeyDownHandler(e) {
+  const key = e.key;
+ 
+  if (key === codes[index]) {
+    index++;
+ 
+    if (index === codes.length) {
+      alert("What do you think you're doing!?");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
 }
